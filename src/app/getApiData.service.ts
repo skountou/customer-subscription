6a1ec08f2d49f4cf3dd3c9b60b5d752e52ad8c52
skountou <skountou439@gmail.com>
2023-03-14 09:22:37 +0200

    3rd-commit

--- src/app/getApiData.service.ts
+++ src/app/getApiData.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient} from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { User } from "./user.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class getApiDataService{
     constructor(private http:HttpClient) { }
+    
 
-    fetchData(){
-        fetch("../assets/dummy-API-data.json")
-        .then(res => {
-            return res.json()})
+    fetchData(id:number){
+        const customerId = id;
+        return this.http
+        .get<User>(
+            `https://640edc144ed25579dc3e9635.mockapi.io/customer-sub/Customer-Subs/?id=${customerId}`
+        );
+         
     }
 }
